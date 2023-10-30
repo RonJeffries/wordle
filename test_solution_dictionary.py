deb52f132f9335ec441e3cf39f5218b11faa73e4
Ron Jeffries <ronjeffries@acm.org>
2023-10-30 08:25:06 -0400

    remove spike test

--- test_solution_dictionary.py
+++ test_solution_dictionary.py
@@ -9,33 +9,6 @@ from word_collection import WordCollection
 
 class TestSolutionDictionary:
 
-    def test_simple(self):
-        all_guesses = WordCollection.from_file("valid_combined.txt")
-        all_solutions = WordCollection.from_file("valid_solutions.txt")
-        guesses = all_guesses.words[0:10000:500]
-        assert len(guesses) <= 20
-        solutions = all_solutions.words[0:2000:100]
-        assert len(solutions) <= 20
-        print(solutions)
-        solutions_dict = {}  # guess -> dict (score -> [solutions])
-        for guess in guesses:
-            guess_dict = {}  # score -> [solutions]
-            solutions_dict[guess] = guess_dict
-            for solution in solutions:
-                score = guess.score(solution)
-                if not score in guess_dict:
-                    guess_dict[score] = []
-                guess_dict[score].append(solution)
-        print()
-        for solution in solutions_dict:
-            print(solution)
-            for score in solutions_dict[solution]:
-                print(f"    {score:05d}")
-                answers = solutions_dict[solution][score]
-                for answer in answers:
-                    print("        ", answer)
-        # assert False
-
     def test_solution_dictionary(self):
         all_guesses = WordCollection.from_file("valid_combined.txt")
         all_solutions = WordCollection.from_file("valid_solutions.txt")
